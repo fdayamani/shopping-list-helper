c833479ed20f5eb07a3af3f3a49c547c606e546f
Fatema Damani <fatema.damani@gmail.com>
2016-09-27 11:06:40 +0100

    Adds shadow jar dependency to be able to create fat jar containing all dependencies. Fat jar created by running ./gradlew clean shadowJar and lives in infrastructure/build/libs

--- infrastructure/src/main/java/io/github/fdayamani/slh/StdOut.java
+++ infrastructure/src/main/java/io/github/fdayamani/slh/StdOut.java
@@ -1,11 +1,16 @@
 package io.github.fdayamani.slh;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 public class StdOut implements ShoppingDestination {
 
     @Override
     public void outputShoppingList(Set<String> groceryList) {
-        System.out.println(groceryList.toString().replaceAll(",", "\n"));
+        System.out.println(sortedOutput(groceryList));
+    }
+
+    private String sortedOutput(Set<String> groceryList) {
+        return new TreeSet(groceryList).toString().replaceAll(",", "\n");
     }
 }
