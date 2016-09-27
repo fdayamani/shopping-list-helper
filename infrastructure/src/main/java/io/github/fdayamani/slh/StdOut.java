f9390fe1134398a76242379ba9f32a8f61df0923
Fatema Damani <fatema.damani@gmail.com>
2016-09-27 11:34:46 +0100

    Adds ingredients to list in lower case format, and outputs in alphabetical order

--- infrastructure/src/main/java/io/github/fdayamani/slh/StdOut.java
+++ infrastructure/src/main/java/io/github/fdayamani/slh/StdOut.java
@@ -1,16 +1,13 @@
 package io.github.fdayamani.slh;
 
 import java.util.Set;
-import java.util.TreeSet;
 
 public class StdOut implements ShoppingDestination {
 
     @Override
     public void outputShoppingList(Set<String> groceryList) {
-        System.out.println(sortedOutput(groceryList));
-    }
-
-    private String sortedOutput(Set<String> groceryList) {
-        return new TreeSet(groceryList).toString().replaceAll(",", "\n");
+        groceryList.stream()
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .forEach(ingredient -> System.out.println(ingredient));
     }
 }
